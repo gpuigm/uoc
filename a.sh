3789b9b3f60a31a26609c3b733622e357b3b84fc
gpuigm <154664224+gpuigm@users.noreply.github.com>
2023-12-23 12:17:23 +0100

    Update a.sh
    Remove the awk references.

--- a.sh
+++ a.sh
@@ -1,90 +1,55 @@
 #!/bin/bash
-#Nom i cognom de l'alumne: Gabriel Puig Martín
-#Usuari de la UOC de l'alumne: gpuigm
-#Data: 23/12/2023
-
-# URL de descàrrega: https://raw.githubusercontent.com/gpuigm/uoc/main/dataset_practica_2023_2024_s1.zip
-# Obtenim l'URL des de l'entrada.
-
 
 url=$1
 
-# Primer, comprovem si l'entrada és una URL vàlida.
 if [[ $url =~ ^https?:// ]]; then
-  # Si és vàlida, descarreguem l'arxiu ZIP amb wget.
   wget --no-check-certificate -O archivo.zip "$(printf '%s' "$url")" > /dev/null 2>&1
 else
-  cat <<< "Error: L'URL proporcionada no és vàlida."
+  echo "Error: L'URL proporcionada no és vàlida."
   exit 1
 fi
 
-# Comprovar si s'ha descarregat un arxiu ZIP.
 if [ ! -f "./archivo.zip" ]; then
-  cat <<< "Error: No s'ha pogut descarregar cap arxiu ZIP de l'URL proporcionada."
+  echo "Error: No s'ha pogut descarregar cap arxiu ZIP de l'URL proporcionada."
   exit 1
 fi
 
-# Si tot ha anat bé fins ara, descomprimim l'arxiu ZIP i esborra'l.
 unzip -o "./archivo.zip" > /dev/null 2>&1
 rm -rf archivo.zip > /dev/null 2>&1
 
-
-
-# Ara, per a cada arxiu CSV, farem el següent:
 for csv in ./*.csv
 do
-  # Calculem la firma digital MD5.
-  md5=$(md5sum "$csv" | awk '{ print $1 }')
-
-  # Obtenim el nom de l'arxiu sense l'extensió.
+  md5=$(md5sum "$csv" | cut -d' ' -f1)
   csvname=$(basename "$csv" .csv)
-
-  # Determinem la codificació de caràcters.
   charset=$(file -i "$csv" | sed -n 's/.*charset=\(.*\)/\1/p')
-
-  # Contem el nombre de registres.
   records=$(wc -l < "$csv")
-
-  # Contem el nombre de columnes.
   columns=$(head -1 "$csv" | tr ';' '\n' | wc -l)
 
-  # Imprimim la informació obtinguda.
-  cat <<< "MD5: $md5"
-  cat <<< "Nom de l'arxiu: $csvname"
-  cat <<< "Codificació de caràcters: $charset"
-  cat <<< "Nombre total de registres: $records"
-  cat <<< "Nombre total de columnes: $columns"
+  echo "MD5: $md5"
+  echo "Nom de l'arxiu: $csvname"
+  echo "Codificació de caràcters: $charset"
+  echo "Nombre total de registres: $records"
+  echo "Nombre total de columnes: $columns"
 
-  # Per a cada columna, obtenim el nom i el tipus de dades.
-
-IFS=';' read -r -a array <<< "$(head -1 "$csv")"
+  IFS=';' read -r -a array <<< "$(head -1 "$csv")"
   for index in "${!array[@]}"
   do
     column=${array[index]}
-    type=$(awk -F';' -v col=$((index+1)) '{print $col}' "$csv" | awk '
-    {
-      if ($0 + 0 == $0) {
-        printf("%s\n", (int($0) == $0) ? "integer" : "float")
-      } else {
-        printf("string\n")
-      }
-    }' | sort -u | grep -v string)
-    if [[ $type == "" ]]; then
-      if grep -Pq '^(\d{2}/\d{2}/\d{4}|\d{4}-\d{2}-\d{2})$' <(awk -F';' -v col=$((index+1)) '{print $col}' "$csv"); then
-        type="data"
-      elif grep -Pq '^(true|false)$' <(awk -F';' -v col=$((index+1)) '{print $col}' "$csv"); then
-        type="boolean"
-      else
-        type="string"
-      fi
+    type=$(cut -d';' -f$((index+1)) "$csv" | grep -P '^\d+$' > /dev/null && echo "integer" || echo "string")
+    if [[ $type == "string" ]]; then
+      type=$(cut -d';' -f$((index+1)) "$csv" | grep -P '^\d+\.\d+$' > /dev/null && echo "float" || echo "string")
     fi
-    cat <<< "Tipus Columna$((index+1)): $column, $type"
+    if [[ $type == "string" ]]; then
+      type=$(cut -d';' -f$((index+1)) "$csv" | grep -P '^(\d{2}/\d{2}/\d{4}|\d{4}-\d{2}-\d{2})$' > /dev/null && echo "data" || echo "string")
+    fi
+    if [[ $type == "string" ]]; then
+      type=$(cut -d';' -f$((index+1)) "$csv" | grep -P '^(true|false)$' > /dev/null && echo "boolean" || echo "string")
+    fi
+    echo "Tipus Columna$((index+1)): $column, $type"
   done
 
-
-
-  # Finalment, imprimim un separador abans de passar al següent arxiu CSV.
-  cat <<< "                    "
-  cat <<< "********************"
-  cat <<< "                    "
+  echo "                    "
+  echo "********************"
+  echo "                    "
 done
+
